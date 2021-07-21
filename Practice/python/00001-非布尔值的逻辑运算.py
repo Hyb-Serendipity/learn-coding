58d7289d522d340b73b45d5985ebeee61520513d
pohoulong <pohoulong@163.com>
2021-07-21 20:30:15 +0800

    Update 00001-非布尔值的逻辑运算.py

--- Practice/python/00001-非布尔值的逻辑运算.py
+++ Practice/python/00001-非布尔值的逻辑运算.py
@@ -1,5 +1,8 @@
 result1 = True or True
 result2 = 1 or 2
 result3 = 1 and 2
-result4 = 
-print(result1,"\n",result2,"\n",result3)
+# 若第一个是 False 直接返回第二个值
+result4 = False or 0
+result5 = False and 1
+result6 = 1 and False
+print(result1,"\n",result2,"\n",result3,"\n",result4,"\n",result5,"\n",result6)
